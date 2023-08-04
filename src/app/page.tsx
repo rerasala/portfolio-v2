93cf269442e503d29ff89a333fa3140ceb4bd10b
Rohan Erasala <rohan@Rohans-MacBook-Pro.local>
2023-08-04 15:49:25 -0700

    fixed linting error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,11 +11,11 @@ export default function Home() {
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex pt-5">
         <div className="container">
           <div className="pb-5">
-            <h2 className="lg:text-2xl font-semibold font-sans">Oh Hello! I'm</h2>
+            <h2 className="lg:text-2xl font-semibold font-sans">Oh Hello! I&apos;m</h2>
             <h1 className="font-extrabold font-sans text-transparent drop-shadow-2xl text-5xl md:text-6xl bg-clip-text bg-gradient-to-r from-blue-400 to-green-600">Rohan Erasala</h1>
           </div>
           <div className='pt-5 font-mono'>
-            <h3 className='text'>I'm an engineer</h3>
+            <h3 className='text'>I&apos;m an engineer</h3>
             <h3 className='text'>Designer</h3>
             <h3 className='text'>Guitarist</h3>
             <h3 className='text'>and more!</h3>
