e3b3be3bc89e4edf1bdcf2c69a4eac82d7f350d1
Rohan Erasala <rohan@Rohans-MacBook-Pro.local>
2023-08-06 22:15:59 -0700

    fixed hamburger menu margin

--- src/app/navbar/navbar.tsx
+++ src/app/navbar/navbar.tsx
@@ -11,7 +11,7 @@ const Navbar = () => {
 
   return (
     <nav id="nav" className="bg-blue-950 sticky top-0 z-50">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="max-w-7xl ml-0 sm:ml-4 md:ml-8 px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="hidden md:flex justify-start w-full">
             <div className="flex items-baseline space-x-4">
