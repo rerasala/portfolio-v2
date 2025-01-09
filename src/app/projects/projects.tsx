3284ae5a6f9fffc7cb7370b3fcf818e672d615e3
rerasala <rerasala@umich.edu>
2025-01-08 23:47:57 -0500

    resolved build error

--- src/app/projects/projects.tsx
+++ src/app/projects/projects.tsx
@@ -42,7 +42,7 @@ export default function Projects() {
             <div className="px-6 py-4">
                 <div className="font-bold text-xl mb-2 text-black">Sentiment Analysis of NFL Draft threads</div>
                 <p className="text-gray-700 text-base">
-                Can fans really tell who's going to be a great football player? Find out in my research paper <a style={{color: 'blue'}} target="_blank" href="https://publications.waset.org/10013885/tweets-to-touchdowns-predicting-national-football-league-achievement-from-social-media-optimism">here</a>. Received an award for the best presentation.
+                Can fans really tell who&apos;s going to be a great football player? Find out in my research paper <a style={{color: 'blue'}} target="_blank" href="https://publications.waset.org/10013885/tweets-to-touchdowns-predicting-national-football-league-achievement-from-social-media-optimism">here</a>. Received an award for the best presentation.
                 </p>
             </div>
             <div className="px-6 pt-4 pb-2">
